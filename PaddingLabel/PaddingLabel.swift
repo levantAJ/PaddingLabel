6f1ae8f4d00e39c0aed910bdd0f6566a495c4123
levantAJ <sirlevantai@gmail.com>
2018-11-11 09:43:29 +0700

    Create Padding label

---
+++ PaddingLabel/PaddingLabel.swift
@@ -0,0 +1,28 @@
+//
+//  PaddingLabel.swift
+//  PaddingLabel
+//
+//  Created by levantAJ on 11/11/18.
+//  Copyright © 2018 levantAJ. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable public class PaddingLabel: UILabel {
+    
+    @IBInspectable public var topInset: CGFloat = 5.0
+    @IBInspectable public var bottomInset: CGFloat = 5.0
+    @IBInspectable public var leftInset: CGFloat = 7.0
+    @IBInspectable public var rightInset: CGFloat = 7.0
+    
+    override public func drawText(in rect: CGRect) {
+        let insets = UIEdgeInsets.init(top: topInset, left: leftInset, bottom: bottomInset, right: rightInset)
+        super.drawText(in: rect.inset(by: insets))
+    }
+    
+    override public var intrinsicContentSize: CGSize {
+        let size = super.intrinsicContentSize
+        return CGSize(width: size.width + leftInset + rightInset,
+                      height: size.height + topInset + bottomInset)
+    }
+}
