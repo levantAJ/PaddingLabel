4351ad340ff5c32e9df6f28be4059714d489944b
levantAJ <sirlevantai@gmail.com>
2018-11-11 09:40:31 +0700

    Initial Commit

---
+++ PaddingLabel/PaddingLabel.h
@@ -0,0 +1,19 @@
+//
+//  PaddingLabel.h
+//  PaddingLabel
+//
+//  Created by levantAJ on 11/11/18.
+//  Copyright © 2018 levantAJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PaddingLabel.
+FOUNDATION_EXPORT double PaddingLabelVersionNumber;
+
+//! Project version string for PaddingLabel.
+FOUNDATION_EXPORT const unsigned char PaddingLabelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PaddingLabel/PublicHeader.h>
+
+
